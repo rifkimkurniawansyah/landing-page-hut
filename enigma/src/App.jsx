064d7fdeaf31a0f2e36ff09bc5c93f2333fe9553
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-19 21:08:13 +0700

    about us

--- enigma/src/App.jsx
+++ enigma/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import AboutUs from "./component/AboutUs.jsx";
 import Hero from "./component/Hero.jsx";
 import Navbar from "./component/Navbar.jsx";
 // import "./App.css";
@@ -12,6 +13,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <AboutUs />
     </>
   );
 }
