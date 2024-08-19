4cfe16b7f861e863818c422878e414179899ae30
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-08-19 18:09:53 +0700

    responsive Navbar

--- HUT-RI/src/components/pages/HomePages.jsx
+++ HUT-RI/src/components/pages/HomePages.jsx
@@ -7,14 +7,6 @@ export default function HomePage() {
 
     return (
         <div>
-            {/*<header className="flex justify-between items-center p-4 md:p-8 navbar-bg">*/}
-            {/*    <img*/}
-            {/*        src="../src/assets/logo79.png"*/}
-            {/*        alt="Logo 79"*/}
-            {/*        className="w-32 md:w-40 h-auto"*/}
-            {/*    />*/}
-            {/*    <Navbar/>*/}
-            {/*</header>*/}
             <Hero/>
             <FilosofiLogo/>
             <SejarahKemerdekaan/>
