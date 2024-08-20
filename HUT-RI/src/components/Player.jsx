cd1d68fa31028ffd473ed32f54f0c22efd797f76
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-20 10:46:58 +0700

    audio autoplay chrome policy workaround

--- HUT-RI/src/components/Player.jsx
+++ HUT-RI/src/components/Player.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
+
 export default function AudioPlayer() {
+  useEffect(() => {
+    const audioElement = document.getElementById("audio");
+
+    const playAudio = () => {
+      audioElement.play().catch((error) => {
+        console.log("Autoplay was prevented:", error);
+      });
+    };
+
+    window.addEventListener("click", playAudio);
+
+    return () => {
+      window.removeEventListener("click", playAudio);
+    };
+  }, []);
+
   return (
     <div className="fixed bottom-4 right-4 z-50">
       <audio
+        id="audio"
         src="/lagu_kemerdekaan.mp3"
         loop
         controls
         controlsList="nodownload"
+        type="audio/mp3"
       />
     </div>
   );
